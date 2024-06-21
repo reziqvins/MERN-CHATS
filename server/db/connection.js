69b8b474750fdba4398c208d9b4d3dd458c67a27
reziqvins <nreziq@gmail.com>
2024-06-21 20:52:30 +0700

    first commit

---
+++ server/db/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const url = `mongodb+srv://reziqvins:akjjyglc@ecomerce.knchrkf.mongodb.net/?retryWrites=true&w=majority&appName=Ecomerce`;
+// mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.zw6hky5.mongodb.net/?retryWrites=true&w=majority
+mongoose.connect(url, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+}).then(() => console.log('Connected to DB')).catch((e)=> console.log('Error', e))
+
+//akjjyglc
